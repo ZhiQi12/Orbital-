3de8f58a5a793fd4a28ae4dcd06b2050da4ed040
ZhiQi12 <chewzhiqi21@gmail.com>
2022-07-19 17:10:26 +0800

    changed path of model in main.py

--- fe/moderate/main.py
+++ fe/moderate/main.py
@@ -48,8 +48,8 @@ def SIA_analyse_sent(comments):
 #    return RFR_AI_model(scrape_n_posts(MOD, n))
 
 def RFR_AI_model_predict(comments): #input list of strings
-    #PATH = "C:/Orbital/Orbital_Moderate/fe/moderate/RFR_model.sav"
-    PATH = 'https://github.com/ZhiQi12/Orbital-/blob/master/fe/moderate/RFR_model.sav'
+    PATH = "C:/Orbital/Orbital_Moderate/fe/moderate/RFR_model.sav"
+    #PATH = 'https://github.com/ZhiQi12/Orbital-/blob/master/fe/moderate/RFR_model.sav'
     #model = pickle.load(open(PATH, 'rb'))
     model = pickle.load(urllib.request.urlopen(PATH))
     ratings = model.predict(comments)
