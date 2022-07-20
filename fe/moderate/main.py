cfcfe17fdfeaa956ba88b94f057881c6dd6ac058
ZhiQi12 <chewzhiqi21@gmail.com>
2022-07-21 01:23:14 +0800

    tweaked file path when loading model

--- fe/moderate/main.py
+++ fe/moderate/main.py
@@ -63,7 +63,7 @@ def RFR_AI_model_predict(comments): #input list of strings
     print(comments)
     
     #model = cp.load(urlopen(PATH))
-    model = pickle.load(open('fe/moderate/model/RFR_model.sav', 'rb'))
+    model = pickle.load(open('fe/moderate/RFR_model.sav', 'rb'))
     #file = urllib.request.urlopen(PATH)
     #model = pickle.load(urllib.request.urlopen(PATH))
     ratings = model.predict(comments)
