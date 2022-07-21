f4a72becf127fa9f0cef2ab1b841878226c8b0d1
ZhiQi12 <chewzhiqi21@gmail.com>
2022-07-21 14:20:08 +0800

    added slug statements and increase max length

--- fe/moderate/models.py
+++ fe/moderate/models.py
@@ -4,12 +4,14 @@ from django.db import models
 class Module(models.Model):
     code = models.CharField(max_length = 10)
     rating = models.FloatField(default = 0.0)
-    comment1 = models.CharField(max_length = 200, default = "")
-    comment2 = models.CharField(max_length = 200, default = "")
-    comment3 = models.CharField(max_length = 200, default = "")
+    comment1 = models.CharField(max_length = 350, default = "")
+    comment2 = models.CharField(max_length = 350, default = "")
+    comment3 = models.CharField(max_length = 350, default = "")
     searched = models.IntegerField(default = 1)
-    emotions = models.CharField(max_length = 200, default = "1.0,1.0,1.0,1.0,1.0")
+    emotions = models.CharField(max_length = 350, default = "1.0,1.0,1.0,1.0,1.0")
+    slug = models.SlugField(max_length=350)
 
 class Issue(models.Model):
     code = models.CharField(max_length = 10, default = "")
-    message = models.CharField(max_length=200, default = "")
+    message = models.CharField(max_length=350, default = "")
+    slug = models.SlugField(max_length=350)
