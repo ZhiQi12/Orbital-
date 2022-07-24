ed846a7e9ad224686d9500e84727d4490dbbd6ae
ZhiQi12 <chewzhiqi21@gmail.com>
2022-07-25 00:44:47 +0800

    moderateapp to moderate

--- moderate/moderate/settings.py
+++ moderate/moderate/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'moderate.apps.ModerateConfig',
-    'moderateapp.apps.ModerateappConfig'
+    'moderateapp.apps.ModerateConfig'
 ]
 
 MIDDLEWARE = [
